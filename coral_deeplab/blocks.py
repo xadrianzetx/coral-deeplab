bf68cfb709a507f0176614fddd9814d3aeffbb56
Adrian Zuber <xadrianzetx@gmail.com>
2021-03-08 19:51:58 +0100

    add block defs

---
+++ coral_deeplab/blocks.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+from coral_deeplab.layers import UpSampling2D
+
+
+def inverted_res_block():
+    pass
+
+
+def deeplab_aspp_module():
+    pass
+
+
+def deeplab_decoder():
+    pass
