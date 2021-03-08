b7410d9b3747800d7bcd06b474ccc8bd778c4889
Adrian Zuber <xadrianzetx@gmail.com>
2021-03-08 20:28:38 +0100

    implemented deeplab decoder

--- coral_deeplab/blocks.py
+++ coral_deeplab/blocks.py
@@ -11,5 +11,37 @@ def deeplab_aspp_module():
     pass
 
 
-def deeplab_decoder():
-    pass
+def deeplab_decoder(inputs: tf.Tensor, skip_con: tf.Tensor,
+                    n_classes: int, bn_epsilon: float) -> tf.Tensor:
+    """
+    """
+
+    if n_classes > 50:
+        print('Warning - model might not compile'
+              ' due to upsampling of large tensors.'
+              ' Consider decreasing number of'
+              ' segmentation classes.')
+
+    skip = tf.keras.layers.Conv2D(48, 1, padding='same', use_bias=False,
+                                  name='project_0')(skip_con)
+    skip = tf.keras.layers.BatchNormalization(epsilon=bn_epsilon)(skip)
+    skip = tf.keras.layers.ReLU()(skip)
+
+    aspp_up = UpSampling2D(size=(4, 4), interpolation='bilinear')(inputs)
+    x = tf.keras.layers.Concatenate()([aspp_up, skip])
+
+    x = tf.keras.layers.SeparableConv2D(256, 3, padding='same',
+                                        use_bias=False)(x)
+    x = tf.keras.layers.BatchNormalization(epsilon=bn_epsilon)(x)
+    x = tf.keras.layers.ReLU()(x)
+
+    x = tf.keras.layers.SeparableConv2D(256, 3, padding='same',
+                                        use_bias=False)(x)
+    x = tf.keras.layers.BatchNormalization(epsilon=bn_epsilon)(x)
+    x = tf.keras.layers.ReLU()(x)
+
+    outputs = tf.keras.layers.SeparableConv2D(n_classes, 3, padding='same',
+                                              use_bias=False)(x)
+    outputs = UpSampling2D(size=(4, 4), interpolation='bilinear')(outputs)
+
+    return outputs
