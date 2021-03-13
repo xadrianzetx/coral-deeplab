b8831f51a8bfc868559145c0942abb3a69dabc89
Adrian Zuber <xadrianzetx@gmail.com>
2021-03-13 16:43:16 +0100

    add fake dataset generator for compile tests

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -1,6 +1,18 @@
+import sys
 import unittest
+import numpy as np
 
 import coral_deeplab as cdl
+
+
+def fake_dataset_generator(shape, n_iter):
+    def dataset():
+        for _ in range(n_iter):
+            data = np.random.randn(*shape)
+            data *= (1 / 255)
+            batch = np.expand_dims(data, axis=0)
+            yield [batch.astype(np.float32)]
+    return dataset
 
 
 class TestCoralDeepLabV3Plus(unittest.TestCase):
@@ -25,6 +37,10 @@ class TestCoralDeepLabV3Plus(unittest.TestCase):
         with self.assertRaises(ValueError):
             input_shape = (512, 512, 3)
             cdl.applications.CoralDeepLabV3Plus(input_shape)
+
+    @unittest.skipUnless(sys.platform.startswith('linux'), 'linux required')    
+    def test_edgetpu_compiles(self):
+        pass
 
 
 if __name__ == '__main__':
