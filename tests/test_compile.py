ee81478c2ced570218a8d961f18d2577ce96b9bc
Adrian Zuber <xadrianzetx@gmail.com>
2021-03-13 17:04:18 +0100

    add compiler pipeline

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -1,6 +1,10 @@
+import os
 import sys
 import unittest
+import subprocess
 import numpy as np
+import tensorflow as tf
+from uuid import uuid4
 
 import coral_deeplab as cdl
 
@@ -13,6 +17,31 @@ def fake_dataset_generator(shape, n_iter):
             batch = np.expand_dims(data, axis=0)
             yield [batch.astype(np.float32)]
     return dataset
+
+
+def quantize_and_compile(model, dataset):
+
+    # setup tflite converter
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.representative_dataset = dataset
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    # quantize
+    quantized = converter.convert()
+    model_name = f'{uuid4()}.tflite'
+    model_path = os.path.join(os.getcwd(), model_name)
+    open(model_path, 'wb').write(quantized)
+
+    # compile
+    cmd = ['edgetpu_compiler', '-s', model_path]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    p.wait()
+    stdout = p.stdout.read()
+
+    return stdout.decode()
 
 
 class TestCoralDeepLabV3Plus(unittest.TestCase):
