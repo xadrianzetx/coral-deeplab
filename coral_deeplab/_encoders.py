82acacbc961057e04dad9d89a0cb4ba306fe2f42
Adrian Zuber <xadrianzetx@gmail.com>
2021-06-12 17:41:01 +0200

    implemented variable depth multiplier in MobileNetv2

--- coral_deeplab/_encoders.py
+++ coral_deeplab/_encoders.py
@@ -35,7 +35,7 @@ from coral_deeplab._blocks import (
 )
 
 
-def mobilenetv2(inputs: tf.Tensor) -> tf.Tensor:
+def mobilenetv2(inputs: tf.Tensor, alpha: float = 1.0) -> tf.Tensor:
     """Implements modified version of MobileNetV2.
 
     Last three inverted res block use dilated conv
@@ -47,13 +47,16 @@ def mobilenetv2(inputs: tf.Tensor) -> tf.Tensor:
     inputs : tf.Tensor
         Input tensor.
 
+    alpha : float, default=1.0
+        Depth multiplier.
+
     Returns
     -------
     outputs : tf.Tensor
         Output tensor.
     """
 
-    x = Conv2D(32, 3, 2, padding='same', use_bias=False,
+    x = Conv2D(int(alpha * 32), 3, 2, padding='same', use_bias=False,
                kernel_regularizer=tf.keras.regularizers.L2(L2),
                name='Conv')(inputs)
     x = BatchNormalization(momentum=BN_MOMENTUM, name='Conv/BatchNorm')(x)
@@ -66,29 +69,35 @@ def mobilenetv2(inputs: tf.Tensor) -> tf.Tensor:
                            name='expanded_conv/depthwise/BatchNorm')(x)
     x = ReLU(6, name='expanded_conv/depthwise/relu')(x)
 
-    x = Conv2D(16, 1, padding='same', use_bias=False,
+    x = Conv2D(int(alpha * 16), 1, padding='same', use_bias=False,
                kernel_regularizer=tf.keras.regularizers.L2(L2),
                name='expanded_conv/project')(x)
     x = BatchNormalization(momentum=BN_MOMENTUM,
                            name='expanded_conv/project/BatchNorm')(x)
 
-    x = inverted_res_block(x, 24, expand_channels=96, strides=2, block_num=1)
-    x = inverted_res_block(x, 24, expand_channels=144, skip=True, block_num=2)
-    x = inverted_res_block(x, 32, expand_channels=144, strides=2, block_num=3)
-    x = inverted_res_block(x, 32, expand_channels=192, skip=True, block_num=4)
-    x = inverted_res_block(x, 32, expand_channels=192, skip=True, block_num=5)
-    x = inverted_res_block(x, 64, expand_channels=192, strides=2, block_num=6)
-    x = inverted_res_block(x, 64, expand_channels=384, skip=True, block_num=7)
-    x = inverted_res_block(x, 64, expand_channels=384, skip=True, block_num=8)
-    x = inverted_res_block(x, 64, expand_channels=384, skip=True, block_num=9)
-    x = inverted_res_block(x, 96, expand_channels=384, block_num=10)
-    x = inverted_res_block(x, 96, expand_channels=576, skip=True, block_num=11)
-    x = inverted_res_block(x, 96, expand_channels=576, skip=True, block_num=12)
-    x = inverted_res_block(x, 160, expand_channels=576, block_num=13)
+    x = inverted_res_block(x, int(alpha * 24), expand_channels=int(alpha * 96), strides=2, block_num=1)
+    x = inverted_res_block(x, int(alpha * 24), expand_channels=int(alpha * 144), skip=True, block_num=2)
+    x = inverted_res_block(x, int(alpha * 32), expand_channels=int(alpha * 144), strides=2, block_num=3)
+    x = inverted_res_block(x, int(alpha * 32), expand_channels=int(alpha * 192), skip=True, block_num=4)
+    x = inverted_res_block(x, int(alpha * 32), expand_channels=int(alpha * 192), skip=True, block_num=5)
+    x = inverted_res_block(x, int(alpha * 64), expand_channels=int(alpha * 192), strides=2, block_num=6)
+    x = inverted_res_block(x, int(alpha * 64), expand_channels=int(alpha * 384), skip=True, block_num=7)
+    x = inverted_res_block(x, int(alpha * 64), expand_channels=int(alpha * 384), skip=True, block_num=8)
+    x = inverted_res_block(x, int(alpha * 64), expand_channels=int(alpha * 384), skip=True, block_num=9)
+    x = inverted_res_block(x, int(alpha * 96), expand_channels=int(alpha * 384), block_num=10)
+    x = inverted_res_block(x, int(alpha * 96), expand_channels=int(alpha * 576), skip=True, block_num=11)
+    x = inverted_res_block(x, int(alpha * 96), expand_channels=int(alpha * 576), skip=True, block_num=12)
+    x = inverted_res_block(x, int(alpha * 160), expand_channels=int(alpha * 576), block_num=13)
 
     # modified
-    x = inverted_res_block(x, 160, expand_channels=960, skip=True, dilation=2, block_num=14)
-    x = inverted_res_block(x, 160, expand_channels=960, skip=True, dilation=2, block_num=15)
-    outputs = inverted_res_block(x, 320, expand_channels=960, dilation=2, block_num=16)
+    x = inverted_res_block(x, int(alpha * 160),
+                           expand_channels=int(alpha * 960),
+                           skip=True, dilation=2, block_num=14)
+    x = inverted_res_block(x, int(alpha * 160),
+                           expand_channels=int(alpha * 960),
+                           skip=True, dilation=2, block_num=15)
+    outputs = inverted_res_block(x, int(alpha * 320),
+                                 expand_channels=int(alpha * 960),
+                                 dilation=2, block_num=16)
 
     return outputs
