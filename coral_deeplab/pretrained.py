8e402c31693f0f21a7878d3529556d4a9548f9f2
Adrian Zuber <xadrianzetx@gmail.com>
2021-06-26 14:42:14 +0200

    add artifacts for DeepLabV3Plus

--- coral_deeplab/pretrained.py
+++ coral_deeplab/pretrained.py
@@ -38,6 +38,16 @@ class KerasModel(MLModel):
         'filename': 'deeplabv3_mnv2_dm05_voc_tainaug_os16.h5',
         'checksum': '36e1e957a62848451db92c53abc1d7d7'
     }
+    DEEPLAB_V3_PLUS_DM1 = {
+        'origin': '191I3qg-S245BD8aX1jfGF2Yy3H9-1A1l',
+        'filename': 'deeplabv3plus_mnv2_dm1_voc_trainaug_os4.h5',
+        'checksum': 'c43f0acf3a256daa237da66ecedb4565'
+    }
+    DEEPLAB_V3_PLUS_DM05 = {
+        'origin': '17wv_wRPZMnj2s_y_nol8whkwda0C2O77',
+        'filename': 'deeplabv3plus_mnv2_dm05_voc_trainaug_os4.h5',
+        'checksum': 'e3e002c39716bc54f966bae657fc2f78'
+    }
 
 
 class EdgeTPUModel(MLModel):
@@ -50,4 +60,14 @@ class EdgeTPUModel(MLModel):
         'origin': '1bukSOJf8JL_RSQwrCIypvzxxamEhO9cV',
         'filename': 'deeplabv3_mnv2_dm05_voc_tainaug_os16_edgetpu.tflite',
         'checksum': '2d3ad50d08c12dba4d5ea61f59bb0b79'
+    }
+    DEEPLAB_V3_PLUS_DM1 = {
+        'origin': '1-2U13RHX5b-h7rIfhxovpxeC4c6DNA8r',
+        'filename': 'deeplabv3plus_mnv2_dm1_voc_trainaug_os4_edgetpu.tflite',
+        'checksum': '3ad64d967a3e526d7df4a3b3a8a60f8a'
+    }
+    DEEPLAB_V3_PLUS_DM05 = {
+        'origin': '1DJ11luO0SMU69egtPShP-4-rSVYki-HP',
+        'filename': 'deeplabv3plus_mnv2_dm05_voc_trainaug_os4_edgetpu.tflite',
+        'checksum': 'abab0449b81be44efcfab4cacccc7f1b'
     }
